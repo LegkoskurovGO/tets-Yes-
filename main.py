231c258983999cc609b9475259dc62596adc63cb
LegkoskurovGO <georgelegko@gmail.com>
2023-11-08 13:27:53 +0300

    Навёл порядок

--- main.py
+++ main.py
@@ -2,54 +2,36 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 # import catboost
+
+'''Скачиваем данные'''
 train = pd.read_csv('train_amazon.csv', index_col=0).astype(object)
 test = pd.read_csv('test_amazon.csv', index_col=0).astype(object).drop('id', axis=1)
 
 data = pd.concat([train, test], axis=0, ignore_index=True)
 
-print(data.columns)
-
-
-data.info()
-# print(f'Кол-во{sum(data.isna())}')
-
+'''Предварительный просмотр данных'''
+# print(data.columns)
+# data.info()
 # print(train.head())
-print(data.shape)
+print(train.shape)
 # print(train.columns)
 # train.info()
 # print(train.nunique())
 # print(f'\nДлина датасета: {train.shape[0]}')
 
-# train_1 = train.query('ACTION == 1').reset_index(drop=True)
-# train_0 = train.query('ACTION == 0').reset_index(drop=True)
-# tmp_1 = train_1.nunique()
-# tmp_0 = train_0.nunique()
-
-# res = pd.concat([tmp_1, tmp_0], axis=1)
-# print(res.head(10))
-# 
-# sns.scatterplot(x='ROLE_CODE', y='ROLE_TITLE', data=train)
-# plt.show()
-
-
-train.info()
-print(f'Nunique: train\n{train.nunique()}\n\n')
-
+'''Число уникальных значений сгруппированных по ACTION из train'''
 train_1 = train.query('ACTION == 1').reset_index(drop=True)
 train_0 = train.query('ACTION == 0').reset_index(drop=True)
 
-res = pd.concat([train.nunique(), train_0.nunique(), train_1.nunique()], axis=1)
-res.columns = ['All', 'A0', 'A1']
-print(res)
+res_train = pd.concat([train.nunique(), train_0.nunique(), train_1.nunique()], axis=1)
+res_train.columns = ['All', 'A0', 'A1']
+print(f'Only train: \n{res_train}')
 
-print()
+'''Число уникальных значений сгруппированных по ACTION из data'''
+data_1 = data.query('ACTION == 1').reset_index(drop=True)
+data_0 = data.query('ACTION == 0').reset_index(drop=True)
 
-# print('\n'*4)
+res_data = pd.concat([data.nunique(), data_0.nunique(), data_1.nunique()], axis=1)
+res_data.columns = ['All', 'A0', 'A1']
+print(f'All data: \n{res_data}')
 
-
-train_1 = data.query('ACTION == 1').reset_index(drop=True)
-train_0 = data.query('ACTION == 0').reset_index(drop=True)
-
-res = pd.concat([data.nunique(), train_0.nunique(), train_1.nunique()], axis=1)
-res.columns = ['All', 'A0', 'A1']
-print(res)
